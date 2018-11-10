a2668cdc4f53464af9aa2bad0aa663fe7bf70cee
Sergey Kozlov <mail.sergey.kozlov@gmail.com>
2018-11-10 18:29:22 +0700

    Cell

--- core/src/main/java/com/github/skozlov/mines/core/Cell.java
+++ core/src/main/java/com/github/skozlov/mines/core/Cell.java
@@ -2,36 +2,67 @@ package com.github.skozlov.mines.core;
 
 import java.util.function.Consumer;
 
-public class Cell {
+public abstract class Cell {
+	private Cell(){}
+
 	public boolean isMined() {
-
+		return false;
 	}
 
-	public void fold(Consumer<Mined> mined, Consumer<Free> free) {
-		//todo
+	public abstract void fold(Consumer<Mined> mined, Consumer<Free> free);
+
+	public static final class Mined extends Cell {
+		public static final Mined INSTANCE = new Mined();
+
+		private Mined(){
+			super();
+		}
+
+		@Override
+		public boolean isMined() {
+			return true;
+		}
+
+		@Override
+		public void fold(Consumer<Mined> mined, Consumer<Free> free) {
+			mined.accept(this);
+		}
 	}
 
-	@Override
-	public int hashCode() {
+	public static final class Free extends Cell {
+		private final int neighborMineNumber;
 
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-
-	}
-
-	public static class Mined extends Cell {
-		public static final Mined INSTANCE = ;
-	}
-
-	public static class Free extends Cell {
 		public Free(int neighborMineNumber) {
-			//todo
+			super();
+			if (neighborMineNumber < 0 || neighborMineNumber > 8){
+				throw new IllegalArgumentException(String.format(
+					"Incorrect neighbor mine number %d",
+					neighborMineNumber
+				));
+			}
+			this.neighborMineNumber = neighborMineNumber;
 		}
 
 		public int getNeighborMineNumber() {
+			return neighborMineNumber;
+		}
 
+		@Override
+		public void fold(Consumer<Mined> mined, Consumer<Free> free) {
+			free.accept(this);
+		}
+
+		@Override
+		public int hashCode() {
+			return neighborMineNumber;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			return obj == this
+				|| obj != null
+					&& obj.getClass().equals(getClass())
+					&& ((Free)obj).neighborMineNumber == neighborMineNumber;
 		}
 	}
 }
