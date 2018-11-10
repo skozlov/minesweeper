44458f8b63bad43404d69501b07639ff5c3d0cd3
Sergey Kozlov <mail.sergey.kozlov@gmail.com>
2018-11-10 12:06:08 +0700

    SwingUtils

--- gui/src/main/java/com/github/skozlov/mines/gui/SwingUtils.java
+++ gui/src/main/java/com/github/skozlov/mines/gui/SwingUtils.java
@@ -1,7 +1,15 @@
 package com.github.skozlov.mines.gui;
 
+import javax.swing.*;
+
 public class SwingUtils {
 	public static void executeInBackground(Runnable task){
-		//todo
+		new SwingWorker<Object, Object>() {
+			@Override
+			protected Object doInBackground() {
+				task.run();
+				return null;
+			}
+		}.execute();
 	}
 }
