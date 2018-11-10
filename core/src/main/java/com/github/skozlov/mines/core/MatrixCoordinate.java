9e9c95e11016a8cbd9e7d2c67821099350ecf92c
Sergey Kozlov <mail.sergey.kozlov@gmail.com>
2018-11-10 18:46:15 +0700

    MatrixCoordinate

--- core/src/main/java/com/github/skozlov/mines/core/MatrixCoordinate.java
+++ core/src/main/java/com/github/skozlov/mines/core/MatrixCoordinate.java
@@ -1,32 +1,52 @@
 package com.github.skozlov.mines.core;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-public class MatrixCoordinate {
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
+public final class MatrixCoordinate {
+	private final int rowIndex;
+	private final int columnIndex;
+
 	public MatrixCoordinate(int rowIndex, int columnIndex) {
-		//todo
-	}
-
-	@Override
-	public int hashCode() {
-
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-
+		if (rowIndex < 0){
+			throw new IllegalArgumentException(String.format("Negative row index %d", rowIndex));
+		}
+		if (columnIndex < 0){
+			throw new IllegalArgumentException(String.format("Negative column index %d", columnIndex));
+		}
+		this.rowIndex = rowIndex;
+		this.columnIndex = columnIndex;
 	}
 
 	public int getRowIndex() {
-
+		return rowIndex;
 	}
 
 	public int getColumnIndex() {
-
+		return columnIndex;
 	}
 
 	public List<MatrixCoordinate> getNeighbors(MatrixDimension matrixDimension) {
-
+		checkFor(matrixDimension);
+		int minRowIndex = max(0, rowIndex - 1);
+		int minColumnIndex = max(0, columnIndex - 1);
+		int maxRowIndex = min(matrixDimension.getRowNumber() - 1, rowIndex + 1);
+		int maxColumnIndex = min(matrixDimension.getColumnNumber() - 1, columnIndex + 1);
+		List<MatrixCoordinate> neighbors = new ArrayList<>(
+			(maxRowIndex - minRowIndex + 1) * (maxColumnIndex - minColumnIndex + 1) - 1
+		);
+		for (int rowIndex = minRowIndex; rowIndex <= maxRowIndex; rowIndex++){
+			for (int columnIndex = minColumnIndex; columnIndex <= maxColumnIndex; columnIndex++){
+				if (!(rowIndex == this.rowIndex && columnIndex == this.columnIndex)){
+					neighbors.add(new MatrixCoordinate(rowIndex, columnIndex));
+				}
+			}
+		}
+		return neighbors;
 	}
 
 	public void checkFor(MatrixDimension dimension){
@@ -43,5 +63,22 @@ public class MatrixCoordinate {
 				columnIndex, columnNumber
 			));
 		}
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(rowIndex, columnIndex);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this){
+			return true;
+		}
+		if (obj == null || !obj.getClass().equals(getClass())){
+			return false;
+		}
+		MatrixCoordinate that = (MatrixCoordinate) obj;
+		return this.rowIndex == that.rowIndex && this.columnIndex == that.columnIndex;
 	}
 }
