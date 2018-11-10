bdb12c09ba5dd1eaa5c665047b6c8d0ce7df3971
Sergey Kozlov <mail.sergey.kozlov@gmail.com>
2018-11-10 18:50:40 +0700

    MatrixDimension

--- core/src/main/java/com/github/skozlov/mines/core/MatrixDimension.java
+++ core/src/main/java/com/github/skozlov/mines/core/MatrixDimension.java
@@ -1,30 +1,66 @@
 package com.github.skozlov.mines.core;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Consumer;
 
-public class MatrixDimension {
+public final class MatrixDimension {
+	private final int rowNumber;
+	private final int columnNumber;
+	private final List<MatrixCoordinate> coordinates;
+
 	public MatrixDimension(int rowNumber, int columnNumber) {
-		//todo
+		if (rowNumber <= 0){
+			throw new IllegalArgumentException(String.format("Illegal row number %d", rowNumber));
+		}
+		if (columnNumber <= 0){
+			throw new IllegalArgumentException(String.format("Illegal column number %d", columnNumber));
+		}
+		this.rowNumber = rowNumber;
+		this.columnNumber = columnNumber;
+		coordinates = new ArrayList<>(rowNumber * columnNumber);
+		for (int rowIndex = 0; rowIndex < rowNumber; rowIndex++){
+			for (int columnIndex = 0; columnIndex < columnNumber; columnIndex++){
+				coordinates.add(new MatrixCoordinate(rowIndex, columnIndex));
+			}
+		}
 	}
 
 	public int getRowNumber() {
-
+		return rowNumber;
 	}
 
 	public int getColumnNumber() {
-
+		return columnNumber;
 	}
 
 	public int getCellNumber() {
-
+		return rowNumber * columnNumber;
 	}
 
 	public void forEachCoordinate(Consumer<MatrixCoordinate> action){
-		//todo
+		coordinates.forEach(action);
 	}
 
 	public List<MatrixCoordinate> coordinatesToList() {
+		return new ArrayList<>(coordinates);
+	}
 
+	@Override
+	public int hashCode() {
+		return Objects.hash(rowNumber, columnNumber);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this){
+			return true;
+		}
+		if (obj == null || !obj.getClass().equals(getClass())){
+			return false;
+		}
+		MatrixDimension that = (MatrixDimension) obj;
+		return this.rowNumber == that.rowNumber && this.columnNumber == that.columnNumber;
 	}
 }
