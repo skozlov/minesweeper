1cfe67ae904d8f2979645be55905ac00e3ee976d
Sergey Kozlov <mail.sergey.kozlov@gmail.com>
2018-11-10 12:01:30 +0700

    FontUtils

--- gui/src/main/java/com/github/skozlov/mines/gui/FontUtils.java
+++ gui/src/main/java/com/github/skozlov/mines/gui/FontUtils.java
@@ -1,13 +1,21 @@
 package com.github.skozlov.mines.gui;
 
 import java.awt.*;
+import java.awt.font.TextAttribute;
+import java.util.Map;
 
 public class FontUtils {
 	public static Font strikeOn(Font font) {
-
+		//noinspection unchecked
+		Map<TextAttribute, Object> attributes = (Map<TextAttribute, Object>) font.getAttributes();
+		attributes.put(TextAttribute.STRIKETHROUGH, true);
+		return new Font(attributes);
 	}
 
 	public static Font strikeOff(Font font) {
-
+		//noinspection unchecked
+		Map<TextAttribute, Object> attributes = (Map<TextAttribute, Object>) font.getAttributes();
+		attributes.put(TextAttribute.STRIKETHROUGH, false);
+		return new Font(attributes);
 	}
 }
